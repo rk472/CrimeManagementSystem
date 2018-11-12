2adb66df2f0d308fd790806effbcdadb20aca6a7
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-11-12 23:12:33 +0530

    Designs Done

--- app/src/main/java/com/smarttersstudio/rescuenation/MenuActivity.java
+++ app/src/main/java/com/smarttersstudio/rescuenation/MenuActivity.java
@@ -1,24 +1,48 @@
 package com.smarttersstudio.rescuenation;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+    int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CALL_PHONE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
     }
 
     public void goToCrime(View view) {
-        startActivity(new Intent(this,CrimeActivity.class));
+        startActivity(new Intent(this, CrimeActivity.class));
     }
 
     public void goToMissings(View view) {
-        startActivity(new Intent(this,MissingActivity.class));
+        startActivity(new Intent(this, MissingActivity.class));
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
